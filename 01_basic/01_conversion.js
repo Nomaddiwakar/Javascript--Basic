70363c854517d636aaf18c1d1a865219a406dcda
Nomaddiwakar <142746171+Nomaddiwakar@users.noreply.github.com>
2026-07-18 12:54:03 +0000

    comparison config

--- 01_basic/01_conversion.js
+++ 01_basic/01_conversion.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+// let score = "33abc"
 //Nan = Even though its name says "Not-a-Number", its data type is actually number.
 // console.log(typeof score);
 // console.log(typeof(score));
@@ -11,25 +11,25 @@ let score = "33abc"
 // "33abc" => NaN
 // true => 1; False => 0
 
-let isloggedIn = 1
+// let isloggedIn = 1
 
-let booleanisloggedin = Boolean(isloggedIn)
-console.log(booleanisloggedin);
+// let booleanisloggedin = Boolean(isloggedIn)
+// console.log(booleanisloggedin);
 
 // 1 => true; 0=> false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber)
 
 // ************************************operation************************************
 
-let value = 3
-let negValue = -value
+// let value = 3
+// let negValue = -value
 // console.log(negValue);
 
 // console.log(2+2);
@@ -40,15 +40,16 @@ let negValue = -value
 // console.log(2%2);
 
 
-let str1 = "nomad"
-let str2 = "diwakar"
-let str3 = (str1 + str2)
-console.log(str3);
+// let str1 = "nomad"
+// let str2 = "diwakar"
+// let str3 = (str1 + str2)
+// console.log(str3);
 
 
-let a=4;
-let b =5;
-let c= a= b;
-console.log(c)
+// let a=4;
+// let b =5;
+// let c= a= b;
+// console.log(c)
 
- 
+************************Comparison Operators************************
+
